fd8f0c960116e4b0c8b33525020fc09003b72c0d
Jacek Wójciak <fr.banka@gmail.com>
2024-09-10 21:45:10 +0200

    up

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,7 @@
+import "./Home.css";
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+export const Home = () => {
+  return <div>HUJ</div>;
+};
--- src/pages/pagesData.tsx
+++ src/pages/pagesData.tsx
@@ -1,11 +1,17 @@
 import { routerType } from "../types/router.types";
 import { Intro } from "./Intro/Intro";
+import { Home } from "./Home/Home";
 
 const pagesData: routerType[] = [
   {
-    path: "",
+    path: "intro",
     element: <Intro />,
     title: "intro",
+  },
+  {
+    path: "",
+    element: <Home />,
+    title: "Home",
   },
 ];
 
