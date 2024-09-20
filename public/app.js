32452d1f5efdbc0e06555b39afe58a8a3a40dcfc
Jacek Wójciak <fr.banka@gmail.com>
2024-09-20 15:18:11 +0200

    edit script slider

--- public/app.js
+++ public/app.js
@@ -16,16 +16,16 @@ export default function onClickButton() {
   document.getElementById("root").classList.remove("root_disabled");
   document.getElementById("root").classList.add("root_enabled");
   document.getElementById("body").appendChild(script);
-}
+};
 export default function changeBg() {
   document
     .getElementById("logo_hover")
     .classList.replace("logo_hover", "logo2");
   document.getElementById("background").id = "background2";
-}
+};
 export default function changeBgBack() {
   document
     .getElementById("logo_hover")
     .classList.replace("logo2", "logo_hover");
   document.getElementById("background2").id = "background";
-}
+};
